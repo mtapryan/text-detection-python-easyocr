2d28caf7ca6d523290cabd35a0fc0753d088a0f8
Rumion Apryano <apryanthamrinaldimuhammad@gmail.com>
2024-09-24 23:09:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -99,5 +99,5 @@ def process_image(filepath, uploads_dir):
 def uploaded_file(filename):
     return send_from_directory('uploads', filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Tambahkan baris ini

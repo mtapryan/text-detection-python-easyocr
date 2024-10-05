084053ca31d7e597d62e06a289f4e29bde9ff16d
Rumion Apryano <apryanthamrinaldimuhammad@gmail.com>
2024-10-05 07:58:02 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Upload from "./components/Upload";
 import Results from "./components/Results";
 import ResultsMultiple from "./components/ResultsMultiple";
+import "./App.css"; // Import style CSS
 
 const App = () => {
   const [results, setResults] = useState([]);
   const [groupedResults, setGroupedResults] = useState({});
   const [resultImage, setResultImage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleUpload = async (files) => {
     const formData = new FormData();
@@ -16,20 +18,34 @@ const App = () => {
     }
 
     try {
+      setIsLoading(true); // Show loading overlay when uploading images
       const response = await fetch("http://localhost:3001/upload_multiple", {
         method: "POST",
         body: formData,
       });
       const data = await response.json();
       setGroupedResults(data.grouped_results);
+      setResults(data.results);
+      setResultImage(data.result_image);
     } catch (error) {
       console.error("Error uploading images:", error);
+    } finally {
+      setIsLoading(false); // Hide loading overlay once upload is finished
     }
   };
 
   return (
     <Router>
       <div className="App">
+        {/* Show loading overlay while images are being uploaded */}
+        {isLoading && (
+          <div id="loadingOverlay" className="loading-overlay">
+            <div id="loadingText" className="loading-text">
+              Loading, please wait...
+            </div>
+          </div>
+        )}
+
         <Routes>
           <Route exact path="/" element={<Upload onUpload={handleUpload} />} />
           <Route
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./../App.css";
 
 const Upload = ({ onUpload }) => {
   const [multipleImages, setMultipleImages] = useState([]);
